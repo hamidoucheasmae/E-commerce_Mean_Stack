e1095da1c5b4313c3ba6b9334f1a82b6262cff30
hamidoucheasmae <76485278+hamidoucheasmae@users.noreply.github.com>
2022-03-30 10:40:14 +0000

    10

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,16 @@
 const express =require('express');
 const app =express();
 
-app.get('/',(req, res)=>{
+
+require('dotenv/config');
+
+const api =process.env.API_URL;
+
+app.get(api+'/',(req, res)=>{
     res.send('hello api ');
 })
 
 app.listen(3000, ()=>{
+    console.log(api)
     console.log('server is runnig http://localhost:3000')
 })
