1539973b833b238b7c9ee3960ecf18cc7ac5a516
hamidoucheasmae <76485278+hamidoucheasmae@users.noreply.github.com>
2022-03-30 11:06:23 +0000

    12. Logging API Requests

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,17 @@
 const express =require('express');
 const app =express();
+const bodyParser =require('body-parser');
+const morgan =require('morgan');
 
+
+// middleware
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
 
 require('dotenv/config');
 
 const api =process.env.API_URL;
 
-// middleware
-app.use(express.json());
 
 app.get(`${api}/products`,(req, res)=>{
     const product ={
