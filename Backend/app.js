316b39abadf8e326be169aec14f7c51ff76e4288
hamidoucheasmae <76485278+hamidoucheasmae@users.noreply.github.com>
2022-04-01 10:33:25 +0000

    15. TEST - Read Write Data to Database Using api

--- Backend/app.js
+++ Backend/app.js
@@ -15,20 +15,26 @@ const api =process.env.API_URL;
 
 const productSchema =mongoose.Schema ({
     name: String,
-    image: String,
-    countInStock: Number,
+    image :{
+        type: String,
+        required: true
+    },
+    countInStock: {
+        type: Number,
+        required: true
+    }
 })
 
 const Product =mongoose.model('Product',productSchema);
 
 
-app.get(`${api}/products`,(req, res)=>{
-    const product ={
-        id: 1,
-        name :'hair dresser',
-        image :'some_url',
+app.get(`${api}/products`,async(req, res)=>{
+    const productList = await Product.find();
+
+    if(!productList) {
+        res.status(500).json({success: false});
     }
-    res.send(product);
+    res.send(productList);
 })
 
 app.post(`${api}/products`,(req, res)=>{
@@ -43,8 +49,7 @@ app.post(`${api}/products`,(req, res)=>{
        res.status(500).json({
        error: err,
        success:false
-
-        })
+         })
    })
  
 })
